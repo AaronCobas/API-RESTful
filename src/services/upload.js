89eed6d58fb0bd6d360c18ded76e690a84497e55
Aaron Andrew Cobas <aaroncobas2017@gmail.com>
2022-11-07 15:48:45 -0300

    API RESTful

--- src/services/upload.js
+++ src/services/upload.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import __dirname from "../utils.js";
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,__dirname+"/public/images")
+        cb(null,__dirname+"/public/thumbnail")
     },
     filename:function(req,file,cb){
         cb(null,Date.now()+"-"+file.originalname)
